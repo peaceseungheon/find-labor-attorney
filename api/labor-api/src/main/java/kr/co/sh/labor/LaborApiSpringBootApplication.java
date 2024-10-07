e90dd5500e2e612f9e0d1e31b794d90d9279d89f
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-10-07 15:25:43 +0900

    스프링 부트 실행 클래스에서 @ComponentScan 제거
    - 실행 클래스가 루트 패키지에 존재하므로 없어도 컴포넌트 인식 가능

--- api/labor-api/src/main/java/kr/co/sh/labor/LaborApiSpringBootApplication.java
+++ api/labor-api/src/main/java/kr/co/sh/labor/LaborApiSpringBootApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({
-    "kr.co.sh.labor.labor_api",
-    "kr.co.sh.labor.mysql_jpa",
-    "kr.co.sh.labor.labor_core"
-})
 public class LaborApiSpringBootApplication {
 
     public static void main(String[] args) {
